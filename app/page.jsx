c295e4b03d236767c48607edd8d28eb359f78fe1
Mustafa <mustafa_9611@hotmail.com>
2023-06-11 15:06:29 +0200

    added feed, form and profile

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,19 @@
-import Feed from "@components/feed"
+import Feed from "@components/Feed";
 
-const Home = () => {
-  return (
-    <section className = "w-full flex-center flex-col">
-        <h1 className = "head_text text-center ">
-            Prompts Share
-            <br className = "max-md:hidden" />
-            <span className = "orange_gradient text-center" >
-                AI generated prompts for you to share
-            </span>
-        </h1>
-        <p className = "desc text-center">
-            Prompts Share is a place for you to share your prompts and get feedback from others.
-        </p>
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
 
-        <Feed />
+    <Feed />
+  </section>
+);
 
-    </section>
-  )
-}
-
-export default Home
+export default Home;
